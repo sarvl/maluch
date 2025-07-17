61cf1ed23ddd3e078950b8eacb6d527e824e1a44
Racuun <lukaszkmiecikdodtkowe@gmail.com>
2025-07-17 21:43:54 +0200

    add csr to alu

---
+++ Hard/rtl/types.sv
@@ -0,0 +1,16 @@
+package Types;
+
+    typedef struct packed {
+        logic [3:0] opcode;
+        logic imm_present;
+        logic [2:0] funct;
+    } instr_t;
+
+    typedef struct packed {
+        logic Sign;
+        logic Overflow;
+        logic Carry;
+        logic Zero;
+    } csr_t;
+
+endpackage
