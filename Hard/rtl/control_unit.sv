104e59765faf4412dccf2d5873d02f53453d2f5f
xKraZx <107442332+xKraZx@users.noreply.github.com>
2026-02-09 22:05:34 +0100

    Add support for RET instruction in control_unit

--- Hard/rtl/control_unit.sv
+++ Hard/rtl/control_unit.sv
@@ -58,6 +58,7 @@ module control_unit (
     localparam OPCODE_PUSH = 4'b1100;
     localparam OPCODE_PULL = 4'b1101;
     localparam OPCODE_CALL = 4'b1010;
+    localparam OPCODE_RET  = 4'b1011;
 
     // Instruction pointer driver
     always_comb begin
@@ -67,6 +68,8 @@ module control_unit (
             instr_pointer_ctrl = i.imm_valid ? i.imm : reg_out2;
         end else if (i.opcode == OPCODE_CALL) begin
             instr_pointer_ctrl = i.imm_valid ? i.imm : reg_out2;
+        end else if (i.opcode == OPCODE_RET) begin
+            instr_pointer_ctrl = mem_ctrl_data_r;
         end
     end
 
@@ -113,6 +116,10 @@ module control_unit (
             addr_out2 = i.src_reg;
             sp_w_en = 1;
             sp_in = alu_ret;
+        end else if (i.opcode == OPCODE_RET) begin
+            addr_out1 = REG_SP;
+            sp_w_en = 1;
+            sp_in = alu_ret;
         end
     end
 
@@ -155,6 +162,8 @@ module control_unit (
             mem_ctrl_addres = alu_ret;
             mem_ctrl_data_w = instr_pointer_seq;
             mem_ctrl_write_en = 1;
+        end else if (i.opcode == OPCODE_RET) begin
+            mem_ctrl_addres = reg_out1;
         end
     end
 
@@ -182,6 +191,10 @@ module control_unit (
             src1 = reg_out1;
             src2 = 16'h0001;
             alu_ctrl = 3'b001;
+        end else if (i.opcode == OPCODE_RET) begin
+            src1 = reg_out1;
+            src2 = 16'h0001;
+            alu_ctrl = 3'b000;
         end
     end
 
