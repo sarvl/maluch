d6b2b3040e7e99c4a876698f8c3733c7e22693ec
xKraZx <107442332+xKraZx@users.noreply.github.com>
2026-02-10 13:50:21 +0100

    Code refactore in control_unit

--- Hard/rtl/control_unit.sv
+++ Hard/rtl/control_unit.sv
@@ -49,7 +49,7 @@ module control_unit (
     localparam REG_SP = 4'b0010;
 
     // Opcodes
-    localparam OPCODE_BRANCHING = 4'b010x;
+    localparam OPCODE_BRANCHING = 4'b010?;
     localparam OPCODE_MOV = 4'b0001;
     localparam OPCODE_ALU = 4'b0010;
     localparam OPCODE_CMP_TEST = 4'b0011;
@@ -62,17 +62,22 @@ module control_unit (
     localparam OPCODE_CALL = 4'b1010;
     localparam OPCODE_RET  = 4'b1011;
 
-    // Instruction pointer driver
+    // Program counter driver
     always_comb begin
         instr_pointer_ctrl = 16'h0000;
 
-        if (i.opcode ==? OPCODE_BRANCHING) begin
-            instr_pointer_ctrl = i.imm_valid ? i.imm : reg_out2;
-        end else if (i.opcode == OPCODE_CALL) begin
-            instr_pointer_ctrl = i.imm_valid ? i.imm : reg_out2;
-        end else if (i.opcode == OPCODE_RET) begin
-            instr_pointer_ctrl = mem_ctrl_data_r;
-        end
+        case (i.opcode) inside
+            OPCODE_BRANCHING: begin
+                instr_pointer_ctrl = i.imm_valid ? i.imm : reg_out2;
+            end
+            OPCODE_CALL: begin
+                instr_pointer_ctrl = i.imm_valid ? i.imm : reg_out2;
+            end
+            OPCODE_RET: begin
+                instr_pointer_ctrl = mem_ctrl_data_r;
+            end
+            default: ;
+        endcase
     end
 
     // Registers driver
@@ -85,59 +90,74 @@ module control_unit (
         sp_w_en = 0;
         sp_in = 16'h0000;
 
-        if (i.opcode ==? OPCODE_BRANCHING) begin
-            addr_out2 = i.src_reg;
-        end else if (i.opcode == OPCODE_MOV) begin
-            addr_out2 = i.src_reg;
-            addr_in = i.dest_reg;
-            reg_in = i.imm_valid ? i.imm : reg_out2;
-            reg_w_en = 1;
-        end else if (i.opcode == OPCODE_ALU) begin
-            addr_out1 = i.dest_reg;
-            addr_out2 = i.src_reg;
-            addr_in = i.dest_reg;
-            reg_in = alu_ret;
-            reg_w_en = 1;
-        end else if (i.opcode == OPCODE_CMP_TEST &&
-                     i.funct inside {3'b001, 3'b010}) begin
-            addr_out1 = i.dest_reg;
-            addr_out2 = i.src_reg;
-        end else if (i.opcode == OPCODE_STW) begin
-            addr_out1 = i.dest_reg;
-            addr_out2 = i.src_reg;
-        end else if (i.opcode == OPCODE_LDW) begin
-            addr_out2 = i.src_reg;
-            addr_in = i.dest_reg;
-            reg_in = mem_ctrl_data_r;
-            reg_w_en = 1;
-        end else if (i.opcode == OPCODE_OUT) begin
-            addr_out2 = i.src_reg;
-        end else if (i.opcode == OPCODE_IN) begin
-            addr_in = i.dest_reg;
-            reg_in = io_data_r;
-            reg_w_en = 1;
-        end else if (i.opcode == OPCODE_PUSH) begin
-            addr_out1 = REG_SP;
-            addr_out2 = i.src_reg;
-            sp_w_en = 1;
-            sp_in = alu_ret;
-        end else if (i.opcode == OPCODE_PULL) begin
-            addr_out1 = REG_SP;
-            addr_in = i.dest_reg;
-            reg_in = mem_ctrl_data_r;
-            reg_w_en = 1;
-            sp_w_en = 1;
-            sp_in = alu_ret;
-        end else if (i.opcode == OPCODE_CALL) begin
-            addr_out1 = REG_SP;
-            addr_out2 = i.src_reg;
-            sp_w_en = 1;
-            sp_in = alu_ret;
-        end else if (i.opcode == OPCODE_RET) begin
-            addr_out1 = REG_SP;
-            sp_w_en = 1;
-            sp_in = alu_ret;
-        end
+        case (i.opcode) inside
+            OPCODE_BRANCHING: begin
+                addr_out2 = i.src_reg;
+            end
+            OPCODE_MOV: begin
+                addr_out2 = i.src_reg;
+                addr_in = i.dest_reg;
+                reg_in = i.imm_valid ? i.imm : reg_out2;
+                reg_w_en = 1;
+            end
+            OPCODE_ALU: begin
+                addr_out1 = i.dest_reg;
+                addr_out2 = i.src_reg;
+                addr_in = i.dest_reg;
+                reg_in = alu_ret;
+                reg_w_en = 1;
+            end
+            OPCODE_CMP_TEST: begin
+                if (i.funct inside {3'b001, 3'b010}) begin
+                    addr_out1 = i.dest_reg;
+                    addr_out2 = i.src_reg;
+                end
+            end
+            OPCODE_STW: begin
+                addr_out1 = i.dest_reg;
+                addr_out2 = i.src_reg;
+            end
+            OPCODE_LDW: begin
+                addr_out2 = i.src_reg;
+                addr_in = i.dest_reg;
+                reg_in = mem_ctrl_data_r;
+                reg_w_en = 1;
+            end
+            OPCODE_OUT: begin
+                addr_out2 = i.src_reg;
+            end
+            OPCODE_IN: begin
+                addr_in = i.dest_reg;
+                reg_in = io_data_r;
+                reg_w_en = 1;
+            end
+            OPCODE_PUSH: begin
+                addr_out1 = REG_SP;
+                addr_out2 = i.src_reg;
+                sp_w_en = 1;
+                sp_in = alu_ret;
+            end
+            OPCODE_PULL: begin
+                addr_out1 = REG_SP;
+                addr_in = i.dest_reg;
+                reg_in = mem_ctrl_data_r;
+                reg_w_en = 1;
+                sp_w_en = 1;
+                sp_in = alu_ret;
+            end
+            OPCODE_CALL: begin
+                addr_out1 = REG_SP;
+                addr_out2 = i.src_reg;
+                sp_w_en = 1;
+                sp_in = alu_ret;
+            end
+            OPCODE_RET: begin
+                addr_out1 = REG_SP;
+                sp_w_en = 1;
+                sp_in = alu_ret;
+            end
+            default: ;
+        endcase
     end
 
     // IO driver
@@ -147,14 +167,18 @@ module control_unit (
         io_r_en = 0;
         io_data_w = 16'h0000;
 
-        if (i.opcode == OPCODE_OUT) begin
-            io_addr = i.funct;
-            io_w_en = 1;
-            io_data_w = i.imm_valid ? i.imm : reg_out2;
-        end else if (i.opcode == OPCODE_IN) begin
-            io_addr = i.funct;
-            io_r_en = 1;
-        end
+        case (i.opcode) inside
+            OPCODE_OUT: begin
+                io_addr = i.funct;
+                io_w_en = 1;
+                io_data_w = i.imm_valid ? i.imm : reg_out2;
+            end
+            OPCODE_IN: begin
+                io_addr = i.funct;
+                io_r_en = 1;
+            end
+            default: ;
+        endcase
     end
 
     // Memory controller driver
@@ -163,25 +187,33 @@ module control_unit (
         mem_ctrl_data_w = 16'h0000;
         mem_ctrl_write_en = 0;
 
-        if (i.opcode == OPCODE_STW) begin
-            mem_ctrl_addres = i.imm_valid ? i.imm : reg_out2;
-            mem_ctrl_data_w = reg_out1;
-            mem_ctrl_write_en = 1;
-        end else if (i.opcode == OPCODE_LDW) begin
-            mem_ctrl_addres = i.imm_valid ? i.imm : reg_out2;
-        end else if (i.opcode == OPCODE_PUSH) begin
-            mem_ctrl_addres = alu_ret;
-            mem_ctrl_data_w = i.imm_valid ? i.imm : reg_out2;
-            mem_ctrl_write_en = 1;
-        end else if (i.opcode == OPCODE_PULL) begin
-            mem_ctrl_addres = reg_out1;
-        end else if (i.opcode == OPCODE_CALL) begin
-            mem_ctrl_addres = alu_ret;
-            mem_ctrl_data_w = instr_pointer_seq;
-            mem_ctrl_write_en = 1;
-        end else if (i.opcode == OPCODE_RET) begin
-            mem_ctrl_addres = reg_out1;
-        end
+        case (i.opcode) inside
+            OPCODE_STW: begin
+                mem_ctrl_addres = i.imm_valid ? i.imm : reg_out2;
+                mem_ctrl_data_w = reg_out1;
+                mem_ctrl_write_en = 1;
+            end
+            OPCODE_LDW: begin
+                mem_ctrl_addres = i.imm_valid ? i.imm : reg_out2;
+            end
+            OPCODE_PUSH: begin
+                mem_ctrl_addres = alu_ret;
+                mem_ctrl_data_w = i.imm_valid ? i.imm : reg_out2;
+                mem_ctrl_write_en = 1;
+            end
+            OPCODE_PULL: begin
+                mem_ctrl_addres = reg_out1;
+            end
+            OPCODE_CALL: begin
+                mem_ctrl_addres = alu_ret;
+                mem_ctrl_data_w = instr_pointer_seq;
+                mem_ctrl_write_en = 1;
+            end
+            OPCODE_RET: begin
+                mem_ctrl_addres = reg_out1;
+            end
+            default: ;
+        endcase
     end
 
     // ALU driver
@@ -191,34 +223,43 @@ module control_unit (
         src2 = 16'h0000;
         csr_flags_we = 0;
 
-        if (i.opcode == OPCODE_ALU) begin
-            alu_ctrl = i.funct;
-            src1 = reg_out1;
-            src2 = i.imm_valid ? i.imm : reg_out2;
-            csr_flags_we = 1;
-        end else if (i.opcode == OPCODE_CMP_TEST &&
-                     i.funct inside {3'b001, 3'b010}) begin
-            alu_ctrl = i.funct;
-            src1 = reg_out1;
-            src2 = i.imm_valid ? i.imm : reg_out2;
-            csr_flags_we = 1;
-        end else if (i.opcode == OPCODE_PUSH) begin
-            src1 = reg_out1;
-            src2 = 16'h0001;
-            alu_ctrl = 3'b001;
-        end else if (i.opcode == OPCODE_PULL) begin
-            src1 = reg_out1;
-            src2 = 16'h0001;
-            alu_ctrl = 3'b000;
-        end else if (i.opcode == OPCODE_CALL) begin
-            src1 = reg_out1;
-            src2 = 16'h0001;
-            alu_ctrl = 3'b001;
-        end else if (i.opcode == OPCODE_RET) begin
-            src1 = reg_out1;
-            src2 = 16'h0001;
-            alu_ctrl = 3'b000;
-        end
+        case (i.opcode) inside
+            OPCODE_ALU: begin
+                alu_ctrl = i.funct;
+                src1 = reg_out1;
+                src2 = i.imm_valid ? i.imm : reg_out2;
+                csr_flags_we = 1;
+            end
+            OPCODE_CMP_TEST: begin
+                if (i.funct inside {3'b001, 3'b010}) begin
+                    alu_ctrl = i.funct;
+                    src1 = reg_out1;
+                    src2 = i.imm_valid ? i.imm : reg_out2;
+                    csr_flags_we = 1;
+                end
+            end
+            OPCODE_PUSH: begin
+                src1 = reg_out1;
+                src2 = 16'h0001;
+                alu_ctrl = 3'b001;
+            end
+            OPCODE_PULL: begin
+                src1 = reg_out1;
+                src2 = 16'h0001;
+                alu_ctrl = 3'b000;
+            end
+            OPCODE_CALL: begin
+                src1 = reg_out1;
+                src2 = 16'h0001;
+                alu_ctrl = 3'b001;
+            end
+            OPCODE_RET: begin
+                src1 = reg_out1;
+                src2 = 16'h0001;
+                alu_ctrl = 3'b000;
+            end
+            default: ;
+        endcase
     end
 
 endmodule
